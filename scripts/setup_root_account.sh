3af71abada8bf188407e3299be1c1c54789ca2ba
Kyle M. Douglass <kyle.m.douglass@gmail.com>
2019-11-04 21:14:50 +0100

    Configure the WAN

--- scripts/setup_root_account.sh
+++ scripts/setup_root_account.sh
@@ -9,12 +9,25 @@ uci set system.@system[0].ttylogin="1"
 uci set system.@system[0].hostname="${HOSTNAME}"
 uci commit system
 
+# WAN
+uci del network.wan
+uci del network.wan6
+
+uci set network.wan=interface
+uci set network.wan.proto=dhcp
+uci set network.wan.ifname="${WAN_IFNAME}"
+uci set network.wan6=interface
+uci set network.wan6.proto=dhcpv6
+uci set network.wan6.ifname="${WAN_IFNAME}"
+uci commit
+
+
 # /etc/hosts
 printf "\n192.168.1.1 %s %s %s\n" \
        "${HOSTNAME}.${DOMAIN}" "${HOSTNAME}" "${HOSTNAME_ALIASES}" >> /etc/hosts
 sed -i "/127.0.0.1 localhost/a127.0.1.1 ${HOSTNAME}.${DOMAIN} ${HOSTNAME}" /etc/hosts
 
-# ssh
+# SSH
 chmod 700 /etc/dropbear/
 chmod 600 /etc/dropbear/authorized_keys
 
