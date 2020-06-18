fd93b053aef8a07e9ad6a4b2762f6f221872bac0
Kyle M. Douglass <kyle.m.douglass@gmail.com>
2020-06-18 09:07:41 +0200

    SR - add emacs python rules and fix document navigation

--- speech-recognition/caster/rules/emacs_python.py
+++ speech-recognition/caster/rules/emacs_python.py
@@ -9,6 +9,10 @@ from castervoice.lib.merge.state.short import R
 
 class EmacsPythonRule(MappingRule):
     mapping = {
+        "deactivate": R(
+            Key("a-x") + Text("pyvenv-deactivate"),
+            rdescript="Deactivate a Python virtual environment"
+        ),
         "work on": R(
             Key("a-x") + Text("pyvenv-workon"),
             rdescript="Activate a Python virtual environment"
