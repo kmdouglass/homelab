2302c4920eaf470ce2fad513fb09eb07ec97435f
Kyle M. Douglass <kyle.m.douglass@gmail.com>
2020-06-28 10:33:18 +0200

    SR - Clean up the emacs grammar and make it more consistent

--- speech-recognition/caster/rules/emacs.py
+++ speech-recognition/caster/rules/emacs.py
@@ -1,44 +1,42 @@
-from dragonfly import Choice, Dictation, MappingRule
+from dragonfly import Choice, Dictation, IntegerRef, MappingRule
 
 from castervoice.lib.actions import Key, Text
 
 from castervoice.lib.ctrl.mgr.rule_details import RuleDetails
-from castervoice.lib.merge.additions import IntegerRefST
 from castervoice.lib.merge.state.short import R
 
 
 class EmacsRule(MappingRule):
     mapping = {
-        "quit e max": R(Key("c-x, c-c")),
-        "execute command": R(Key("a-x")),
-        "execute shell command": R(Key("a-!")),
-        # Files
-        "open file": R(Key("c-x, c-f")),
-        "save file": R(Key("c-x, c-s")),
-        "save as": R(Key("c-x, c-w")),
-        "save all": R(Key("c-x, s")),
-        "revert to file": R(Key("c-x, c-v")),
+        "e max quit": R(Key("c-x, c-c")),
+        "command execute": R(Key("a-x")),
+        "shell command execute": R(Key("a-!")),
         # Buffers
-        "revert buffer": R(Key("a-x") + Text("revert-buffer")),
-        "close buffer": R(Key("c-x, k")),
-        "switch buffer": R(Key("c-x, b")),
-        "move buffer": R(Key("c-x, o")),
-        "split out": R(Key("c-x, 0")),
-        "split restore": R(Key("c-x, 1")),
-        "split horizontal": R(Key("c-x, 2")),
-        "split vertical": R(Key("c-x, 3")),
+        "file open": R(Key("c-x, c-f")),
+        "file save": R(Key("c-x, c-s")),
+        "file save as": R(Key("c-x, c-w")),
+        "file save all": R(Key("c-x, s")),
+        "file revert": R(Key("a-x") + Text("revert-buffer")),
+        "file close": R(Key("c-x, k")),
+        "file switch": R(Key("c-x, b")),
+        "file move": R(Key("c-x, o")),
+        # Windows
+        "pane close": R(Key("c-x, 0")),
+        "pane expand": R(Key("c-x, 1")),
+        "pane split horizontal": R(Key("c-x, 2")),
+        "pane split vertical": R(Key("c-x, 3")),
         # Selections and editing
         "oops": R(Key("c-underscore")),
-        "cancel selection": R(Key("c-g")),
-        "begin selection": R(Key("c-space")),
-        "cut selection": R(Key("c-w")),
+        "selection cancel": R(Key("c-g")),
+        "selection begin": R(Key("c-space")),
+        "selection cut": R(Key("c-w")),
+        "selection copy": R(Key("a-w")),
         "paste": R(Key("c-y")),
-        "copy": R(Key("a-w")),
         "paste number <n>": R(Key("c-x, r, i, %(n)d")),
-        "forward delete": R(Key("c-delete")),
-        "delete word": R(Key("a-backspace")),
-        "forward delete word": R(Key("a-d")),
-        "fill paragraph": R(Key("a-q")),
+        "delete word forward": R(Key("a-d")),
+        "delete word backward": R(Key("a-backspace")),
+        "paragraph wrap": R(Key("a-q")),
+        "paragraph unwrap": R(Key("a-x") + Text("unfill-paragraph") + Key("enter")),
         # Navigation
         "word forward": R(Key("a-f")),
         "word backward": R(Key("a-b")),
@@ -48,22 +46,21 @@ class EmacsRule(MappingRule):
         "paragraph backward": R(Key("a-rbrace")),
         "page forward": R(Key("c-v")),
         "page backward": R(Key("a-v")),
-        "document forward": R(Key("a-rangle")),
-        "document backward": R(Key("a-langle")),
-        "C function forward": R(Key("ac-a")),
-        "C function backward": R(Key("ac-e")),
-        "incremental search": R(Key("c-s")),
-        "incremental reverse": R(Key("c-r")),
-        "query replace": R(Key("a-percent")),
-        "go to line <n>": R(Key("a-g, a-g")),
-        "prior bracket": R(Key("escape:down, c-b, escape:up")),
-        "next bracket": R(Key("escape:down, c-f, escape:up")),
-        # Modes
-        "dear ed": R(Key("c-x, d"), rdescript="Launch dired"),
+        "file forward": R(Key("a-rangle")),
+        "file backward": R(Key("a-langle")),
+        "search forward": R(Key("c-s")),
+        "search backward": R(Key("c-r")),
+        "replace forward": R(Key("a-percent")),
+        "go to line [<line_num>]": R(Key("a-g, a-g") + Text("%(line_num)s")),
+        "bracket forward": R(Key("escape:down, c-f, escape:up")),
+        "bracket backward": R(Key("escape:down, c-b, escape:up")),
         # Help
         "help me <help_item>": R(Key("c-h") + Key("%(help_item)s")),
         # elisp
         "eval sim": R(Key("c-x, c-e")),
+        # Modes
+        "dear ed": R(Key("c-x, d"), rdescript="Launch dired"),
+        "maj it": R(Key("c-x, g"), rdescript="Launch magit"),
     }
     extras = [
         Choice(
@@ -75,11 +72,10 @@ class EmacsRule(MappingRule):
                 "variable": "v"
             }
         ),
-        Dictation("text"),
-        Dictation("mim"),
-        IntegerRefST("n", 1, 1000),
+        IntegerRef("line_num", 1, 9999),
+        IntegerRef("n", 1, 1000),
     ]
-    defaults = {"n": 1, "mim": ""}
+    defaults = {"line_num": "", "n": 1}
 
 
 def get_rule():
