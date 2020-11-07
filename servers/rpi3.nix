799ad020bb55afa73116a42fc918697d06eb38ca
Kyle M. Douglass <kyle.m.douglass@gmail.com>
2020-11-07 17:04:17 +0100

    Configure the Prometheus data source for Grafana

--- servers/rpi3.nix
+++ servers/rpi3.nix
@@ -57,6 +57,19 @@ in
       addr = "0.0.0.0";
       protocol = "http";
       auth.anonymous.enable = true;
+      auth.anonymous.org_role = "Editor";
+
+      provision = {
+        enable = true;
+        datasources = [
+          {
+            name = "Prometheus";
+            isDefault = true;
+            type = "prometheus";
+            url = "http://localhost:9090";
+          }
+        ];
+      };
     };
     openssh = {
       enable = true;
