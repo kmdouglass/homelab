729ac22d0d76155f5b54fe3acf70cdc625049477
Kyle M. Douglass <kyle.m.douglass@gmail.com>
2020-11-27 14:08:02 +0100

    Scrape metrics from the router

--- modules/prometheus.nix
+++ modules/prometheus.nix
@@ -6,21 +6,22 @@
     enable = true;
     scrapeConfigs = [
       {
-        job_name = "prometheus";
-        static_configs = [
-          {
-            targets = [
-              "localhost:9090"
-            ];
-          }
-        ];
-      }
-      {
         job_name = "node";
         static_configs = [
           {
             targets = [
               "localhost:9100"
+              "router:9100"
+            ];
+          }
+        ];
+      }
+      {
+        job_name = "prometheus";
+        static_configs = [
+          {
+            targets = [
+              "localhost:9090"
             ];
           }
         ];
