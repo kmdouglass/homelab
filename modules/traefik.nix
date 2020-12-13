1ef34cd71889b0fac3f1b6fdf5d6f3447542870e
Kyle M. Douglass <kyle.m.douglass@gmail.com>
2020-12-13 10:52:23 +0100

    Clean up the traefik configuration

--- modules/traefik.nix
+++ modules/traefik.nix
@@ -1,36 +1,27 @@
 { ... }:
+let
+
+  staticConfigOptions = {
+    entryPoints.web.address = ":80";
+  };
+
+  dynamicConfigOptions = {
+    http.routers.router-grafana = {
+      rule = "Host(`grafana.kponics.lan`)";
+      service = "grafana";
+    };
+
+    services.grafana.loadBalancer.servers = [ { url = "http://rpi3.kponics.lan:3000"; } ];
+  };
+
+in
+
 {
   networking.firewall.allowedTCPPorts = [ 80 ];
 
   services.traefik = {
     enable = true;
-
-    staticConfigOptions = {
-      entryPoints = {
-        web = {
-          address = ":80";
-        };
-      };
-    };
-
-    dynamicConfigOptions = {
-      http = {
-
-        routers = {
-          router-grafana = {
-            rule = "Host(`grafana.kponics.lan`)";
-            service = "grafana";
-          };
-        };
-
-        services = {
-          grafana = {
-            loadBalancer = {
-              servers = [ { url = "http://rpi3.kponics.lan:3000"; } ];
-            };
-          };
-        };
-      };
-    };
+    inherit staticConfigOptions;
+    inherit dynamicConfigOptions;
   };
 }
